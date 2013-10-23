db1d359f7fe982ff0ff9fdaa69fc75fd555ecc00
Jacob Vallejo <you@jahkeup.com>
2013-10-23 15:04:08 -0400

    Add nova user to user creation script

--- shared/preinstall.sh
+++ shared/preinstall.sh
@@ -8,12 +8,14 @@ sed -i 's/session    required     pam_loginuid.so/#session    required     pam_l
 
 # Create groups
 groupadd -g 220 keystone
+groupadd -g 221 nova
 groupadd -g 223 cinder
 groupadd -g 224 glance
 groupadd -g 226 neutron
 
 # Crea†e users
 useradd -u 220 -g 220 -d /var/lib/keystone keystone
+useradd -u 221 -g 221 -d /var/lib/nova nova
 useradd -u 223 -g 223 -d /var/lib/cinder cinder
 useradd -u 224 -g 224 -d /var/lib/glance glance
 useradd -u 226 -g 226 -d /var/lib/neutron neutron
