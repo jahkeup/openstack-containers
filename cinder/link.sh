4e9b6b3f1fab664c410805c76d2f6542e96354bd
Jacob Vallejo <you@jahkeup.com>
2013-10-23 06:05:58 -0400

    Safety is off.

--- cinder/link.sh
+++ cinder/link.sh
@@ -1,7 +1,7 @@
 service=cinder
 for f in cinder.conf api-paste.ini; do
   rm /etc/$service/$f
-  echo ln -s /srv/c/$f /etc/$service/$f
+  ln -s /srv/c/$f /etc/$service/$f
 done
 
 echo "PRERUN: Linked and/or verified links to real config files."
