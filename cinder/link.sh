e6dcbb53113a42fcf6c31ea317a81b7a9701566d
Jacob Vallejo <you@jahkeup.com>
2013-10-23 06:20:39 -0400

    Fix link path, I'm going to bed.

--- cinder/link.sh
+++ cinder/link.sh
@@ -2,7 +2,7 @@
 service=cinder
 for f in cinder.conf api-paste.ini; do
   rm /etc/$service/$f
-  ln -s /srv/c/$f /etc/$service/$f
+  ln -s /srv/c/$service/$f /etc/$service/$f
 done
 
 echo "PRERUN: Linked and/or verified links to real config files."
