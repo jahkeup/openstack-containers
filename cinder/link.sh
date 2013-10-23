f8f7e564bbad11671485e87a33a2e2c8771d7b9c
Jacob Vallejo <you@jahkeup.com>
2013-10-23 11:10:14 -0400

    Change around the scripts and add a run script

--- cinder/link.sh
+++ cinder/link.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 service=cinder
 for f in cinder.conf api-paste.ini; do
-  rm /etc/$service/$f
-  ln -s /srv/c/$service/$f /etc/$service/$f
+  ln -s -f /srv/c/$service/$f /etc/$service/$f
 done
 
 echo "PRERUN: Linked and/or verified links to real config files."
