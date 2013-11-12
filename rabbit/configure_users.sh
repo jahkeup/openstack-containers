0fac901394441c570ac6d8ad12a45511d559ca8d
Jacob Vallejo <you@jahkeup.com>
2013-11-11 23:55:35 -0500

    Update configure script for cleanliness

--- rabbit/configure_users.sh
+++ rabbit/configure_users.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 # Rabbit must be started to modify users
-rabbitmq-server&
+echo "Starting rabbit server"
+rabbitmq-server 2>&1 >/dev/null &
 SERVER_PID=$!
 
 # Wait for the server to start..
+echo "Waiting to warn up before configure."
 sleep 5;
 
 if [ ! -f /srv/rabbit_passwords.txt ]; then
     echo "Cannot find rabbit password file. Cannot make users." 1>&2
     exit 1
 fi
-
+echo "Processing rabbit users"
 # For each user line entry in the file, create them or change them.
 # The only user that exists by default is guest.
 for line in $(cat /srv/rabbit_passwords.txt); do
     user=$(echo $line | awk -F: '{print $1}')
     pass=$(echo $line | awk -F: '{print $2}')
+    echo "Configuring user $user."
     if [ "$user" = "guest" ]; then
         rabbitmqctl change_password guest "$pass"
     else
